4cfb6f2cdb9fcc87679669b6f2c3ff5af3d5ec00
text2vijay <text2vijay@hotmail.com>
2022-04-12 18:06:29 +0530

    initial commit of app

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+React testing library cheatsheet
+https://testing-library.com/docs/react-testing-library/cheatsheet/
+
+
+Jest DOM Documentation:
+https://github.com/testing-library/jest-dom
+
+user events:
+https://github.com/testing-library/user-event
---
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
+
+test("Initial conditions", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("Checkbox enables button on first click and disables on second click", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
